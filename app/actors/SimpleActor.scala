14dd0513ca8427ccc44efe819e1e4367075c1e42
Mark Wilson <mwilson@processor44.com>
2013-08-15 14:08:49 -0600

    Placeholder for simple actor actions

---
+++ app/actors/SimpleActor.scala
@@ -0,0 +1,18 @@
+package actors
+
+package actors
+
+import akka.actor.{Props, ActorRef, Actor}
+import java.util.Random
+import scala.collection.immutable.Queue
+import scala.collection.JavaConverters._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.libs.Akka
+
+/**
+ * Do simple actor stuff, like a feed a single value every second
+ */
+object SimpleActor {
+
+}
