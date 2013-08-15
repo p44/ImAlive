7a423bc8d41a5e34c080d4fa83d8d6a53a110bfa
Mark Wilson <mwilson@processor44.com>
2013-08-15 14:44:01 -0600

    Shift to Concurrent.broadcast[JsValue] channel/enumerator model for messaging

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,25 +1,37 @@
 package controllers
 
-import play.api.Play.current
-import play.api.libs.concurrent.Akka
-import scala.concurrent.Future
-import play.api.libs.iteratee._
-import play.api.libs.concurrent._
-import play.api.libs.concurrent.Execution.Implicits._
-import play.api.mvc.Action
-import play.api.mvc.AnyContent
-import play.api.mvc.Controller
+import play.api.mvc._
+import play.api.libs.json.JsValue
+import play.api.libs.iteratee.{ Concurrent, Enumeratee, Enumerator }
+import play.api.libs.EventSource
 import play.api.Logger
 
 /**
  * Entry application controller
  */
 object Application extends Controller {
-  
+
+  /**
+   * Central hub for distributing I'm Alive messages
+   *  aliveOut: Enumerator[JsValue]
+   *  aliveChannel: Channel[JsValue]
+   */
+  val (aliveOut, aliveChannel) = Concurrent.broadcast[JsValue] // aliveOut: Enumerator[JsValue] 
+
   /** main page */
-   def index = Action {
-     Ok(views.html.index.render)
-   }
-	
+  def index = Action {
+    Ok(views.html.index.render)
+  }
+  
+  /** Controller action for POSTing I'm Alive messages */
+  def postAliveMessage = Action(parse.json) { request => 
+    val b = request.body
+    aliveChannel.push(b); 
+    Ok 
+  }
+
+  /** Enumeratee for filtering messages based on room */
+  def filtercustomer(customerid: String) = Enumeratee.filter[JsValue] { json: JsValue =>  (json \ "customerid").as[String] == customerid }
+
 }
 
