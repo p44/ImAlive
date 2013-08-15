d1487c6d5615c90bafb9a968da99b6d454268481
Mark Wilson <mwilson@processor44.com>
2013-08-15 15:45:47 -0600

    Added GET for requesting feed by customer id and POST for adding a message to the chanel

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -22,16 +22,48 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index.render)
   }
-  
-  /** Controller action for POSTing I'm Alive messages */
-  def postAliveMessage = Action(parse.json) { request => 
-    val b = request.body
-    aliveChannel.push(b); 
-    Ok 
+
+  /** Controller action for Posting 'I'm Alive' messages */
+  def postAliveMessage = Action { req =>
+    val jsonValue: Option[JsValue] = req.body.asJson
+    Logger.info("postAliveMessage - request.body.asJson " + jsonValue)
+    jsonValue match {
+      case None => {
+        Logger.info("postAliveMessage - Bad request body " + req.body)
+        BadRequest("Could not resolve body as json " + req.body)
+      }
+      case _ => {
+        aliveChannel.push(jsonValue.get);
+        Ok
+      }
+    }
+
   }
 
-  /** Enumeratee for filtering messages based on room */
-  def filtercustomer(customerid: String) = Enumeratee.filter[JsValue] { json: JsValue =>  (json \ "customerid").as[String] == customerid }
+  /** Enumeratee for filtering messages based on customer */
+  def filtercustomer(customerid: String) = Enumeratee.filter[JsValue] { json: JsValue =>
+    val i = (json \ "customerid").as[String]
+    val b = i == customerid
+    Logger.debug("filtercustomer - json id " + i + " customerid " + customerid + " match " + b)
+    b
+  }
+
+  /** Enumeratee for detecting disconnect of the stream */
+  def connDeathWatch(addr: String): Enumeratee[JsValue, JsValue] = {
+    Enumeratee.onIterateeDone { () =>
+      Logger.info(addr + " - imAliveFeed disconnected")
+    }
+  }
+
+  /** Controller action serving activity based on room */
+  def imAliveFeed(customerid: String) = Action { req =>
+    Logger.info(req.remoteAddress + " - imAliveFeed connected")
+    Ok.stream(aliveOut
+      &> filtercustomer(customerid)
+      &> Concurrent.buffer(50)
+      &> connDeathWatch(req.remoteAddress)
+      &> EventSource()).as("text/event-stream")
+  }
 
 }
 
