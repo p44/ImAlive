c96c73cbbd67888c52452a22ca13c5bc4dc56ff2
Mark Wilson <mwilson@processor44.com>
2013-08-19 11:43:56 -0600

    New constants model for shared keys and values between controller and actors

---
+++ app/models/ImAliveConstants.scala
@@ -0,0 +1,10 @@
+package models
+
+object ImAliveConstants {
+  
+  val URL_ROOT = "http://localhost:9000"
+  val URL_IMALIVE = URL_ROOT + "/imalive"
+  val SECURITY_TOKEN_KEY = "client_token"
+  val SIMULATION_SECURITY_TOKEN = "607789TRE37444HGHGHGHGHG099"
+
+}
