8e61d3248fe50f3d9cc1b447e3d7d09e024723dd
Mark Wilson <mwilson@processor44.com>
2013-08-20 12:54:53 -0600

    add handlers for bad request and errors

--- app/Global.scala
+++ app/Global.scala
@@ -1,5 +1,7 @@
 import actors._
 import play.api.GlobalSettings
+import play.api.mvc.Results._
+import play.api.mvc.RequestHeader
 
 object Global extends GlobalSettings {
 
@@ -9,5 +11,13 @@ object Global extends GlobalSettings {
   
   override def onStop(application: play.api.Application) { 
     SimulatorActors.system.shutdown()
+  }
+  
+  override def onBadRequest(request: RequestHeader, error: String) = {
+    BadRequest("Bad Request: " + error)
+  } 
+  
+  override def onError(request: RequestHeader, ex: Throwable) = {
+    InternalServerError(views.html.e500(ex))
   }
 }
