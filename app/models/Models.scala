14fff5d20ca601fbbb4adabd99a3962d6bed1db3
Mark Wilson <mwilson@processor44.com>
2013-08-27 14:35:03 -0600

    Added real lat long values and added customer name to the status message obj

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -10,13 +10,20 @@ object Models {
   lazy val customers: List[Customer] = List[Customer](Customer(cid1, "Govermnet Snoopers"), Customer(cid2, "Hackers Unlimited"))
   lazy val customerDeviceCatalog: Seq[Device] = Seq[Device](
       // Device(id: Long, cid: Long, mac: String, ip: String, lat: Long, long: Long, desc: String, misc: String)
-      Device(1001L, cid1, "0090990F9F99", "222.222.222.222", 1, 1, "Email reader 39", "12,0,66,99,1,1,0"),
-      Device(1002L, cid1, "0090990F9F87", "222.222.222.221", 1, 1, "FB scraper 87", "11,3,62,92,1,0,5"),
-      Device(7001L, cid2, "0040690F8A09", "111.333.333.11", 1, 1, "Constitution X", "100110111101"),
-      Device(7002L, cid2, "0040110F8B77", "111.333.333.14", 1, 1, "Of The People By The People Z", "101110110101")
+      Device(1001L, cid1, "0090990F9F99", "222.222.222.222", 38.907231,-77.036464, "Email reader 39", "12,0,66,99,1,1,0"),
+      Device(1002L, cid1, "0090990F9F87", "222.222.222.221", 38.907231,-77.036464, "FB scraper 87", "11,3,62,92,1,0,5"),
+      Device(7001L, cid2, "0040690F8A09", "111.333.333.11", 40.0176, 105.2797, "Constitution X", "100110111101"),
+      Device(7002L, cid2, "0040110F8B77", "111.333.333.14", 40.0176, 105.2797, "Of The People By The People Z", "101110110101")
       )
       
   def getRandomDeviceFromCatalog: Device =  { customerDeviceCatalog(scala.util.Random.nextInt(customerDeviceCatalog.size)) }
+  def getCustomerName(cid: Long): Option[String] = {
+    val h: Option[Customer] = customers.filter(_.id == cid).headOption
+    h.isDefined match {
+      case false => None
+      case _ => Some(h.get.name)
+    }
+  }
 
 }
 
@@ -48,7 +55,7 @@ object Customer {
 }
 
 /** */
-case class Device(id: Long, cid: Long, mac: String, ip: String, lat: Long, long: Long, desc: String, misc: String)
+case class Device(id: Long, cid: Long, mac: String, ip: String, lat: Double, long: Double, desc: String, misc: String)
 object Device {
   implicit val jWriter = Json.writes[Device] // Json.toJson(obj): JsValue
   implicit val jReader = Json.reads[Device] // Json.fromJson[Device](jsval): JsResult[Device] .asOpt Option[Device]
@@ -57,17 +64,18 @@ object Device {
 }
 
 /** */
-case class StatusMessage(id: Long, message: String, ts: Long, tsFormatted: String, cid: Long, mac: String, ip: String, lat: Long, long: Long, info: String)
+case class StatusMessage(id: Long, message: String, ts: Long, tsFormatted: String, cid: Long, cName: String, mac: String, ip: String, lat: Double, long: Double, info: String)
 object StatusMessage {
   implicit val jWriter = Json.writes[StatusMessage] // Json.toJson(obj): JsValue
   implicit val jReader = Json.reads[StatusMessage] // Json.fromJson[StatusMessage](jsval): JsResult[StatusMessage] .asOpt Option[StatusMessage]
 
-  def empty: StatusMessage = { StatusMessage(0L, "", DateTimeUtil.defaultDateMillis, "", 0L, "", "", 0L, 0L, "") }
+  def empty: StatusMessage = { StatusMessage(0L, "", DateTimeUtil.defaultDateMillis, "", 0L, "", "", "", 0L, 0L, "") }
   
   /** build simulated message for a device */
   def simulateFromDevice(d: Device, ts: Long, msg: String): StatusMessage = {
     val tsFormatted = DateTimeUtil.formatLongMillis(ts, DateTimeUtil.DATE_FORMATTER_STD_WITH_TIME)
-    StatusMessage(d.id, msg, ts, tsFormatted, d.cid, d.mac, d.ip, d.lat, d.long, d.desc)
+    val cName = Models.getCustomerName(d.cid).getOrElse("Not Found")
+    StatusMessage(d.id, msg, ts, tsFormatted, d.cid, cName, d.mac, d.ip, d.lat, d.long, d.desc)
   }
 }
 
