62f671dc95bf52a7c3c348b95c7f57052e91cf93
Mark Wilson <mwilson@processor44.com>
2013-08-28 14:44:28 -0600

    Dynamically updated the chart, simple bar chart displaying count of messages by customer.

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -7,25 +7,75 @@ angular.module('imAlive.controllers', ['imAlive.services']).
         $scope.msgs = [];
         $scope.inputText = "";
         $scope.currentCustomer = $scope.customers[0];
-        $scope.count = 0;
-        //$scope.data = [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0]];
-        //$scope.ph3 = $('#placeholder3');
-        //$scope.plot = $.plot($scope.ph3, [$scope.data]); // [{ data: [[0, 0], [0, 0]], bars: {show: true, horizontal: true} }]
-        //$scope.plot = $.plot($scope.ph3).data("plot")
-        
-        var data1 = [ [[0, 1], [1, 5], [2, 2]] ], data2 = [ [[0, 4], [1, 2], [2, 4]] ], curr = 1;
-        $scope.data = data1;
-        
-        /** swap data sets */
-        $scope.change = function(){ 
-           if(curr === 1){ 
-             $scope.data = data2;
-             curr = 2;
-           }else{
-             $scope.data = data1;
-             curr = 1;
-           }
+        $scope.count = 0; // count all messages
+        $scope.d1_0 = [ [1, 0] ];
+        $scope.d1_1 = [ [2, 0] ];
+        $scope.barData = [ $scope.d1_0, $scope.d1_1 ]
+        var color0 = "#F89406";
+        var color1 = "#3A87AD";
+        $scope.data1 = [
+                     {
+                         label: "Government",
+                         data: $scope.barData[0], // $scope.d1_0,
+                         bars: {
+                             show: true,
+                             barWidth: 1,
+                             fill: true,
+                             lineWidth: 1,
+                             order: 1,
+                             fillColor: color0
+                         },
+                         color: color0
+                     },
+                     {
+                         label: "Hackers",
+                         data: $scope.barData[1], // $scope.d1_1,
+                         bars: {
+                             show: true,
+                             barWidth: 1,
+                             fill: true,
+                             lineWidth: 1,
+                             order: 2,
+                             fillColor: color1
+                         },
+                         color: color1
+                     }
+                   ]
+        var options1 = {
+                xaxis: {
+                	tickSize: [1, "each"],
+                	tickLength: 0,
+                	minTickSize: 1,
+                    axisLabel: 'Customers',
+                    axisLabelUseCanvas: true,
+                    axisLabelFontSizePixels: 12,
+                    axisLabelFontFamily: 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
+                    axisLabelPadding: 5
+                },
+                yaxis: {
+                	minTickSize: 1,
+                    axisLabel: 'Count',
+                    axisLabelUseCanvas: true,
+                    axisLabelFontSizePixels: 12,
+                    axisLabelFontFamily: 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
+                    axisLabelPadding: 5
+                },
+                grid: {
+                    hoverable: true,
+                    clickable: false,
+                    borderWidth: 3
+                },
+                series: {
+                    shadowSize: 1
+                }
         };
+
+        /** increment count value for the customer at index specified */
+        $scope.incrementBar = function(i) {
+        	// console.log('incrementBar ' + i)
+        	$scope.data1[i].data[0][1] = $scope.data1[i].data[0][1] + 1
+        	
+        }
 
         /** change current customer, restart EventSource connection */
         $scope.setCurrentCustomer = function (customer) {
@@ -38,36 +88,29 @@ angular.module('imAlive.controllers', ['imAlive.services']).
         /** handle incoming messages: add to messages array */
         $scope.addMsg = function (msg) { 
         	var msgobj = JSON.parse(msg.data)
-        	console.log('addMsg...');
+        	//console.log('addMsg...');
+        	$scope.count += $scope.count + 1
             $scope.$apply(function () { $scope.msgs.push(msgobj); });
         };
 
         /** one time init the chart */
         $scope.initChart = function() {
         	console.log('initChart...');
-        	//$scope.plot.setData($scope.data)
-        	//$scope.plot.draw();
-        	var ph3 = $('#placeholder3');
-        	//var d = $scope.data
-        	$.plot(ph3, [ [[0, 4], [1, 2], [2, 4]] ]);
-        	ph3.show();
+        	var ph = $('#feedchart');
+        	var pts = $scope.points;
+        	var d = $scope.data1;
+        	$.plot(ph, d, options1);
         }
         
         /** handle incoming messages: update chart */
         $scope.updateChart = function (msg) { 
-        	var msgobj = JSON.parse(msg.data)
+        	var msgobj = JSON.parse(msg.data);
         	console.log(msgobj);
-        	console.log('updateChart...');
-        	//$scope.count = $scope.count + 1
-        	//var dataSet = [[0, 0], [1, 6], [2,6], [3, 5], [4, 5], [5, 8]];
-        	//$.plot($scope.ph3).setData(dataSet);
-        	//$.plot($scope.ph3).draw();
-        	$scope.change
-        	//$.plot($scope.ph3, [$scope.data]);
-        	//$scope.ph3.show();
-        	var ph3 = $('#placeholder3');
-        	$.plot(ph3, [ [[0, 1], [1, 5], [2, 2]] ]);
-        	ph3.show();
+        	//console.log('updateChart...');
+        	$scope.incrementBar(msgobj.cid -1);
+        	var ph = $('#feedchart');
+        	var d = $scope.data1;
+        	$.plot(ph, d, options1);
         };
 
         /** start listening on messages from selected customer (or all messages depending on what is implemented below) */
