2d2fa9f805a61dfb04cabc3b2055a4b0e005bfc4
Mark Wilson <mwilson@processor44.com>
2013-08-28 19:59:15 -0600

    Starting to include Google libs

--- project/Build.scala
+++ project/Build.scala
@@ -7,6 +7,8 @@ object ApplicationBuild extends Build {
   val appName         = "ImAlive"
   val appVersion      = "0.1-SNAPSHOT"
 
+  // https://github.com/GoogleCloudPlatform/bigquery-getting-started-java/blob/master/src/pom.xml
+    
   val appDependencies = Seq(
     "com.typesafe.akka" %% "akka-actor" % "2.2.0",
     "com.typesafe.akka" %% "akka-slf4j" % "2.2.0",
@@ -14,7 +16,9 @@ object ApplicationBuild extends Build {
     "org.webjars" % "bootstrap" % "2.3.1",
     "org.webjars" % "flot" % "0.8.0",
     "org.webjars" % "angularjs" % "1.1.5-1",
-    "com.typesafe.akka" %% "akka-testkit" % "2.2.0" % "test"
+    "com.typesafe.akka" %% "akka-testkit" % "2.2.0" % "test" //,
+    //"com.google.api-client" % "google-api-client" % "1.17.0-rc-SNAPSHOT"
+    //"com.google.apis" % "google-api-services-bigquery" % "v2-rev42-1.12.0-beta"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,5 +4,9 @@ logLevel := Level.Warn
 // The Typesafe repository 
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
+// resolvers += "Google" at "https://google-api-client-libraries.appspot.com/mavenrepo/"
+
+// resolvers += "Maven OSS Sonatype" at "https://oss.sonatype.org/content/repositories/snapshots/"
+
 // Use the Play sbt plugin for Play projects
 addSbtPlugin("play" % "sbt-plugin" % "2.1.3")
